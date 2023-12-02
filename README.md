e2774013637cbef5f42269cc055e369985a3eb98
takahiro <takapi854@gmail.com>
2023-12-02 21:16:27 +0900

    setup wasm

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rust-wasm-example
+
+
+### Command
+```shell
+$ cargo install wasm-pack #  wasm cli tool
+$ wasm-pack build --target web # build lib & output pkg (Rust → js)
+```
+
+### Check on Browser
+- open ./index.html on browser
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+// js内の外部関数をRustから呼ぶ
+extern {
+    pub fn alert(s: &str);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+// jsが呼ぶRustの関数
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
 }
